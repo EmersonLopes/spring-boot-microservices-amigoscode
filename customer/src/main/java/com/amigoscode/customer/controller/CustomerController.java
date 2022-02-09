7cbb73cbd85dfdce4a25dcebd86c74e098139159
Emerson Lopes <emersonsilvalopes1@gmail.com>
2022-02-09 16:14:28 -0300

    First endpoint

---
+++ customer/src/main/java/com/amigoscode/customer/controller/CustomerController.java
@@ -0,0 +1,20 @@
+package com.amigoscode.customer.controller;
+
+import com.amigoscode.customer.dto.CustomerRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequestMapping("api/v1/customers")
+@RestController
+public class CustomerController {
+
+
+    @PostMapping
+    public void registerCustomer(@RequestBody CustomerRequest customerRequest) {
+        log.info("New customer registration {}", customerRequest);
+    }
+}
